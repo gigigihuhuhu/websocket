13af7009923a7351ead7f6f65bee6aa2dfca9c9f
“kyungsu” <kyungsu77777@gmail.com>
2024-09-09 16:00:05 +0900

    versionup -> 0.0.5 add redis to maintain user session information

--- src/main/java/com/gigigihuhuhu/websocket/controller/SignalingController.java
+++ src/main/java/com/gigigihuhuhu/websocket/controller/SignalingController.java
@@ -2,6 +2,7 @@ package com.gigigihuhuhu.websocket.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gigigihuhuhu.websocket.service.RedisService;
 import com.gigigihuhuhu.websocket.domain.message.ByeMsgDTO;
 import com.gigigihuhuhu.websocket.domain.message.HelloMsgDTO;
 import lombok.RequiredArgsConstructor;
@@ -26,11 +27,13 @@ public class SignalingController {
 
     private static final Logger logger = LoggerFactory.getLogger(SignalingController.class);
     private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisService redisService;
 
     @MessageMapping("/offer/{clientId}")
     public void offer(@DestinationVariable String clientId, @Payload String message) {
         logger.info("Received offer: {}", clientId, message);
-        String sessionId = clientSessions.get(clientId);
+        //String sessionId = clientSessions.get(clientId);
+        String sessionId = redisService.getData(clientId).toString();
         if (sessionId != null) {
             logger.info("Send offer: {}",sessionId, message);
             this.messagingTemplate.convertAndSendToUser(sessionId, "/queue/offer", message, createHeaders(sessionId));
@@ -40,7 +43,8 @@ public class SignalingController {
     @MessageMapping("/answer/{clientId}")
     public void answer(@DestinationVariable String clientId, @Payload String message) {
         logger.info("Received answer: {}", clientId, message);
-        String sessionId = clientSessions.get(clientId);
+//        String sessionId = clientSessions.get(clientId);
+        String sessionId = redisService.getData(clientId).toString();
         if (sessionId != null) {
             logger.info("Send answer: {}",sessionId, message);
             this.messagingTemplate.convertAndSendToUser(sessionId, "/queue/answer", message, createHeaders(sessionId));
@@ -50,7 +54,8 @@ public class SignalingController {
     @MessageMapping("/candidate/{clientId}")
     public void candidate(@DestinationVariable String clientId, @Payload String message) {
         logger.info("Received candidate: {}",clientId, message);
-        String sessionId = clientSessions.get(clientId);
+        //String sessionId = clientSessions.get(clientId);
+        String sessionId = redisService.getData(clientId).toString();
         if (sessionId != null) {
             logger.info("Send candidate: {}",sessionId, message);
             this.messagingTemplate.convertAndSendToUser(sessionId, "/queue/candidate", message, createHeaders(sessionId));
@@ -62,7 +67,8 @@ public class SignalingController {
     public String hello(@Payload HelloMsgDTO helloMsg, SimpMessageHeaderAccessor headerAccessor) throws JsonProcessingException {
         logger.info("Received hello: {}", helloMsg);
         String sessionId = headerAccessor.getSessionId();
-        clientSessions.put(helloMsg.getClientId(), sessionId);
+        //clientSessions.put(helloMsg.getClientId(), sessionId);
+        redisService.saveData(helloMsg.getClientId(), sessionId);
         return new ObjectMapper().writeValueAsString(helloMsg);
     }
 
@@ -70,7 +76,8 @@ public class SignalingController {
     @SendTo("/topic/bye")
     public String bye(@Payload ByeMsgDTO byeMsg) throws JsonProcessingException {
         logger.info("Received bye: {}", byeMsg);
-        clientSessions.remove(byeMsg.getClientId());
+        //clientSessions.remove(byeMsg.getClientId());
+        redisService.deleteKey(byeMsg.getClientId());
         return new ObjectMapper().writeValueAsString(byeMsg);
     }
 
---
+++ src/main/java/com/gigigihuhuhu/websocket/service/RedisService.java
@@ -0,0 +1,24 @@
+package com.gigigihuhuhu.websocket.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void saveData(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object getData(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteKey(String key) {
+        redisTemplate.delete(key);
+    }
+}
