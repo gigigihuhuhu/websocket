3761836f272ad31079bf00e52ccd013a01587083
“kyungsu” <kyungsu77777@gmail.com>
2024-09-09 18:20:48 +0900

    Spring actuator Health 추가 redis 설정 properties 에서 가져오기

--- src/main/java/com/gigigihuhuhu/websocket/config/RedisConfig.java
+++ src/main/java/com/gigigihuhuhu/websocket/config/RedisConfig.java
@@ -1,14 +1,35 @@
 package com.gigigihuhuhu.websocket.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+    private final Environment environment;
+
+
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        String redisHost = environment.getProperty("spring.redis.host");
+        int redisPort = Integer.parseInt(environment.getProperty("spring.redis.port", "6379"));
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        connectionFactory.afterPropertiesSet();
+        return connectionFactory;
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
